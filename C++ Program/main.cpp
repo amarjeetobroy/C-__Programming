b9f5c3c1d890ba2e928f6d7d8a6a4c973bbffb75
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-23 00:24:46 +0530

    Print a number

---
+++ C++ Program/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int num;
+   cout<<"Enter a number: ";
+   cin>>num;
+   cout<<"your number is: ",cout<<num;
+
+    return 0;
+}
+
+/* Output
+Enter a number: 12345
+your number is: 12345
+*/
