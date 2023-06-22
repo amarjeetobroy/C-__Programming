2ddbe264c6b0676c82fae4b5684c7869456babb3
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-23 00:18:34 +0530

    Introduction C++

---
+++ Introduction/home.cpp
@@ -0,0 +1,65 @@
+/*
+What is C++?
+C++ is a cross-platform language that can be used to create high-performance applications.
+
+C++ was developed by Bjarne Stroustrup, as an extension to the C language.
+
+C++ gives programmers a high level of control over system resources and memory.
+
+The language was updated 4 major times in 2011, 2014, 2017, and 2020 to C++11, C++14, C++17, C++20.
+
+Why Use C++
+C++ is one of the world's most popular programming languages.
+
+C++ can be found in today's operating systems, Graphical User Interfaces, and embedded systems.
+
+C++ is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.
+
+C++ is portable and can be used to develop applications that can be adapted to multiple platforms.
+
+C++ is fun and easy to learn!
+
+As C++ is close to C, C# and Java, it makes it easy for programmers to switch to C++ or vice versa.
+
+Difference between C and C++
+C++ was developed as an extension of C, and both languages have almost the same syntax.
+
+The main difference between C and C++ is that C++ support classes and objects, while C does not.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+C++ Get Started
+To start using C++, you need two things:
+
+A text editor, like Notepad, to write C++ code
+A compiler, like GCC, to translate the C++ code into a language that the computer will understand
+There are many text editors and compilers to choose from. In this tutorial, we will use an IDE (see below).
+
+C++ Install IDE
+An IDE (Integrated Development Environment) is used to edit AND compile the code.
+
+Popular IDE's include Code::Blocks, Eclipse, and Visual Studio. These are all free, and they can be used to both edit and debug C++ code.
+
+Note: Web-based IDE's can work as well, but functionality is limited.
+
+We will use Code::Blocks in our tutorial, which we believe is a good place to start.
+
+You can find the latest version of Codeblocks at http://www.codeblocks.org/. Download the mingw-setup.exe file, which will install the text editor with a compiler.
+
+C++ Quickstart
+Let's create our first C++ file.
+
+Open Codeblocks and go to File > New > Empty File.
+
+Write the following C++ code and save the file as myfirstprogram.cpp (File > Save File as):
+
+// 1st program
+
+myfirstprogram.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+*/
